07ae2a93d5d7d90b957dcf9d72c29b0e7b427164
masRobins <robinson.maasiah@gmail.com>
2020-02-03 00:29:14 -0500

    add to cart funcationality implemented, shop data updated

--- src/components/clothing-site-components/global/_custom-button.jsx
+++ src/components/clothing-site-components/global/_custom-button.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 import '../../component-styles/clothing-site-styles/sign-in-and-sign-up/_custom-button.scss'
 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
-  <button 
-    className={`${isGoogleSignIn ? 'google-sign-in ' : ''}custom-button`} {...otherProps}
-    >
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
+    {...otherProps}
+  >
     {children}
   </button>
 );
--- src/components/clothing-site-components/shopping-page/_collections-preview.jsx
+++ src/components/clothing-site-components/shopping-page/_collections-preview.jsx
@@ -5,14 +5,14 @@ import CollectionItem from './_collections-item.jsx';
 
 import '../../component-styles/clothing-site-styles/shopping-page/_collection-preview.scss'
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title}</h1>
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
